f4205a8761d20e9c94caefadfcff7f97e03b6372
Krystof Davidek <kry.davidek@gmail.com>
2021-10-06 13:45:06 +0200

    Change to yarn, redesign header

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -8,18 +8,29 @@ const classes = new BEMHelper({
 
 export const Header = () => {
   return (
+    // <div {...classes()}>
+    //   <h1 {...classes("title")}>
+    //     <p {...classes("title-part")}>
+    //       <span {...classes("title-highlight")}>Cz</span>ech <span {...classes("title-highlight")}>E</span>lectronic
+    //     </p>
+    //     <p {...classes("title-part")}>
+    //       <span {...classes("title-highlight")}>D</span>erivation <span {...classes("title-highlight")}>D</span>ictonary
+    //     </p>
+    //     <p {...classes("title-part")}>
+    //       (<span {...classes("title-highlight")}>CZEDD</span>)
+    //     </p>
+    //   </h1>
+    // </div>
     <div {...classes()}>
-      <h1 {...classes("title")}>
-        <p {...classes("title-part")}>
-          <span {...classes("title-highlight")}>Cz</span>ech <span {...classes("title-highlight")}>E</span>lectronic
+      <div {...classes("title")}>
+        <h1>
+          <span {...classes("title-highlight")}>CZEDD</span>
+        </h1>
+        <p {...classes("subtitle")}>
+          (<span {...classes("highlight")}>Cz</span>ech <span {...classes("highlight")}>E</span>lectronic{" "}
+          <span {...classes("highlight")}>D</span>erivation <span {...classes("highlight")}>D</span>ictonary)
         </p>
-        <p {...classes("title-part")}>
-          <span {...classes("title-highlight")}>D</span>erivation <span {...classes("title-highlight")}>D</span>ictonary
-        </p>
-        <p {...classes("title-part")}>
-          (<span {...classes("title-highlight")}>CZEDD</span>)
-        </p>
-      </h1>
+      </div>
     </div>
   );
 };
